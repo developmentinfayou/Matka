a0e4b3ed882e6e4d5e16ff4265747ca12ac1d4ea
rahulk67 <rahulkumawat6767@gmail.com>
2026-01-07 17:25:41 +0530

    add commision

--- server/db/connection.js
+++ server/db/connection.js
@@ -12,22 +12,24 @@ dotenv.config();
 //     port: 3306
 // });
 
+// for live pool
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'myuser',
+    password: process.env.DB_PASSWORD || 'StrongPassword123!',
+    database: process.env.DB_NAME || 'matka',
+    port: process.env.DB_PORT || 3306,
+    // dateStrings: true ,
+});
+
+
+// for local devlopment  poll
 // const pool = mysql.createPool({
 //     host: process.env.DB_HOST || 'localhost',
 //     user: process.env.DB_USER || 'root',
-//     password: process.env.DB_PASSWORD || '',
+//     password: process.env.DB_PASSWORD ?? '',
 //     database: process.env.DB_NAME || '888',
 //     port: process.env.DB_PORT || 3306,
-//     // timezone: '+05:30'
 // });
-
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD ?? '',
-    database: process.env.DB_NAME || '888',
-    port: process.env.DB_PORT || 3306,
-});
 
 export default pool;
