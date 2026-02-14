309f295f17ccc153472bda5163f12acdaee765d6
rahulk67 <rahulkumawat6767@gmail.com>
2026-02-14 16:55:18 +0530

    db

--- server/db/connection.js
+++ server/db/connection.js
@@ -13,23 +13,23 @@ dotenv.config();
 // });
 
 // for live pool
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST || 'localhost',
-//     user: process.env.DB_USER || 'myuser',
-//     password: process.env.DB_PASSWORD || 'StrongPassword123!',
-//     database: process.env.DB_NAME || 'matka',
-//     port: process.env.DB_PORT || 3306,
-//     // dateStrings: true ,
-// });
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'myuser',
+    password: process.env.DB_PASSWORD || 'StrongPassword123!',
+    database: process.env.DB_NAME || 'matka',
+    port: process.env.DB_PORT || 3306,
+    // dateStrings: true ,
+});
 
 
 // for local devlopment  poll
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD ?? '',
-    database: process.env.DB_NAME || '888',
-    port: process.env.DB_PORT || 3306,
-});
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST || 'localhost',
+//     user: process.env.DB_USER || 'root',
+//     password: process.env.DB_PASSWORD ?? '',
+//     database: process.env.DB_NAME || '888',
+//     port: process.env.DB_PORT || 3306,
+// });
 
 export default pool;
